640319e236480f0329af0b854cdfbc82aa0d7827
Yuqian Yang <yyang@freewheel.tv>
2017-05-14 22:18:29 +0800

    example for ssl usage

---
+++ examples/security/ssl/gen.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# generate certificates signed by CA
+function gencert(){
+  echo '{"CN":"'$1'","hosts":[""],"key":{"algo":"rsa","size":2048}}' | cfssl gencert -config=ca-config.json -ca=ca.pem -ca-key=ca-key.pem -hostname="$2" - | cfssljson -bare $1
+}
+
+source ./config
+source ./default
+
+# generate self-signed ca
+echo '{"CN":"CA","key":{"algo":"rsa","size":2048}}' | cfssl gencert -initca - | cfssljson -bare ca -
+echo '{"signing":{"default":{"expiry":"43800h","usages":["signing","key encipherment","server auth","client auth"]}}}' > ca-config.json
+
+for m in $MEMBERS
+do
+  gencert $m $MEMBER_ADDR
+done
+
